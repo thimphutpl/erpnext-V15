60206df5bd3789cf19ec53bfa160f488abcd7a5e
jai <jai>
2025-01-03 17:05:02 +0600

    fund req json

--- erpnext/accounts/doctype/fund_requisition/fund_requisition.py
+++ erpnext/accounts/doctype/fund_requisition/fund_requisition.py
@@ -29,7 +29,6 @@ class FundRequisition(Document):
 		reference: DF.Data | None
 		remarks: DF.LongText | None
 		total_amount: DF.Currency
-		workflow_state: DF.Link | None
 	# end: auto-generated types
 	def validate(self):
 		self.assign_cost_center()
