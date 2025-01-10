21f19ca69494e8153699c21bde3c8ae8f1f57b76
jai <jai>
2025-01-10 16:06:48 +0600

    imp works

--- erpnext/accounts/doctype/imprest_advance/imprest_advance.py
+++ erpnext/accounts/doctype/imprest_advance/imprest_advance.py
@@ -123,7 +123,7 @@ class ImprestAdvance(Document):
 		if cint(self.first_advance) == 1:
 			notify_workflow_states(self)
 		if not self.is_opening:
-			self.balance_amount = flt(self.amount)
+			self.db_set('balance_amount', flt(self.amount))
 			self.post_journal_entry()
 
 	def on_cancel(self):
