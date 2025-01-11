9573810c8bfe0c72927ae66aedf2ca24effa833d
jai <jai>
2025-01-11 15:21:22 +0600

    pol receive item

--- erpnext/fleet_management/doctype/pol_receive_item/pol_receive_item.py
+++ erpnext/fleet_management/doctype/pol_receive_item/pol_receive_item.py
@@ -5,4 +5,22 @@
 from frappe.model.document import Document
 
 class POLReceiveItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		allocated_amount: DF.Float
+		amount: DF.Float
+		balance: DF.Float
+		balance_amount: DF.Float
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		pol_advance: DF.Link | None
+		serial_and_batch_bundle: DF.Link | None
+	# end: auto-generated types
 	pass
