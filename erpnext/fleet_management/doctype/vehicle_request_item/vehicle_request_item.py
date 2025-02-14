12c4bbdc72eebe2a5322a6f6307209fb9dfea5ae
Rinzin <rinzinn1996@gmail.com>
2025-02-14 09:16:53 +0000

    updated

--- erpnext/fleet_management/doctype/vehicle_request_item/vehicle_request_item.py
+++ erpnext/fleet_management/doctype/vehicle_request_item/vehicle_request_item.py
@@ -5,4 +5,21 @@
 from frappe.model.document import Document
 
 class VehicleRequestItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		designation: DF.Link | None
+		division: DF.Link | None
+		employee: DF.Link | None
+		employee_name: DF.Data | None
+		name: DF.Int | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+	# end: auto-generated types
 	pass
