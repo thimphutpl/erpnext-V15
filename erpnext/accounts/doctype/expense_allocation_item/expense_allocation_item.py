9f3574990e92c1be23db8db0059fa7033393e97d
Rinzin <rinzinn1996@gmail.com>
2025-02-19 12:54:02 +0000

    changes

--- erpnext/accounts/doctype/expense_allocation_item/expense_allocation_item.py
+++ erpnext/accounts/doctype/expense_allocation_item/expense_allocation_item.py
@@ -6,4 +6,27 @@ from frappe.model.document import Document
 
 
 class ExpenseAllocationItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amount: DF.Currency
+		business_activity: DF.Link
+		equipment_number: DF.Data | None
+		expense_for: DF.Literal["", "HSD", "Hire Charge", "Lubricant", "Operator Allowance", "OAP Salary", "Muster Roll Employee", "GCE", "Overtime Payment", "DFG Soelra", "Thai Salary", "Indian Operators Salary", "Repair and Maintenance", "OJT", "Contract Employee"]
+		expense_type: DF.Data | None
+		id_card: DF.Data | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		project: DF.Link
+		quantity: DF.Data | None
+		rate: DF.Currency
+		remarks: DF.LongText | None
+		time: DF.Float
+	# end: auto-generated types
 	pass
