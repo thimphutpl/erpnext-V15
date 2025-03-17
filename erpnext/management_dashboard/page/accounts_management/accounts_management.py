83d529694725fc4848207e69d4b3be1127053115
jai <jai>
2025-03-17 10:40:25 +0000

    metabase works in page

--- erpnext/management_dashboard/page/accounts_management/accounts_management.py
+++ erpnext/management_dashboard/page/accounts_management/accounts_management.py
@@ -3,16 +3,16 @@
 import jwt
 import time
 
-METABASE_SITE_URL = "http://45.64.248.97/metabase"
-METABASE_SECRET_KEY = "0e3efd8ef72652fccdd15028c401d0c85f30c6abae6f644cbfcf46da0efe2b92"
+METABASE_SITE_URL = "https://erp.ns.bt/metabase"
+METABASE_SECRET_KEY = "610dd072f5f4702a31e558a74f3abe4222d3ac5adad849f09ce73218a0207ebc"
 
 payload = {
-  "resource": {"dashboard": 2},
-  "params": {
-    
-  },
-#   "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+"resource": {"dashboard": 3},
+"params": {
+  
+},
+# "exp": round(time.time()) + (60 * 10) # 10 minute expiration
 }
 token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
 
-iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#bordered=true&titled=true"
+iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#theme=night&bordered=true&titled=true"
