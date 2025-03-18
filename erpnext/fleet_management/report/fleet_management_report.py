0a321c0d6718ba82e0da84649f3774317cfee161
SangaTenzin <tenzinsanga1@gmail.com>
2025-03-18 15:20:51 +0600

    Payment Entry And Fleet Management Report

--- erpnext/fleet_management/report/fleet_management_report.py
+++ erpnext/fleet_management/report/fleet_management_report.py
@@ -37,14 +37,14 @@ def get_pol_transfer(purpose, equipment, date, pol_type=None):
 		total = quantity[0].total
 	return total
 
-def get_pol_consumed_till(equipment, date):
-	if not equipment or not date:
-		frappe.throw("Equipment and Till Date are Mandatory")
-	pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and equipment = %s and to_date <= %s", (equipment, date), as_dict=True)
-	if pol:
-		return pol[0].total
-	else:
-		return 0
+# def get_pol_consumed_till(equipment, date):
+# 	if not equipment or not date:
+# 		frappe.throw("Equipment and Till Date are Mandatory")
+# 	pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and equipment = %s and to_date <= %s", (equipment, date), as_dict=True)
+# 	if pol:
+# 		return pol[0].total
+# 	else:
+# 		return 0
 
 ##
 # Both recieved and issued pols can be queried with this
@@ -68,19 +68,19 @@ def get_pol_between(purpose, equipment, from_date, to_date, pol_type=None, own_c
 ##
 # Get consumed POl as per yardstick
 ##
-# def get_pol_consumed_till(equipment, posting_date, posting_time="23:59:59", filter_dry=None):
-# 	if not equipment or not posting_date:
-# 		frappe.throw("Equipment and Till Date are Mandatory")
-# 	posting_datetime = str(get_datetime(str(posting_date) + ' ' + str(posting_time)))
+def get_pol_consumed_till(equipment, posting_date, posting_time="23:59:59", filter_dry=None):
+	if not equipment or not posting_date:
+		frappe.throw("Equipment and Till Date are Mandatory")
+	posting_datetime = str(get_datetime(str(posting_date) + ' ' + str(posting_time)))
 
-# 	if not filter_dry:
-# 		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and include_km=1 and equipment = %s and cast(concat(to_date, ' ', to_time) as datetime) <= %s", (equipment, str(posting_datetime)), as_dict=True)
-# 	else:
-# 		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and include_km=1 and equipment = %s and rate_type = 'With Fuel' and cast(concat(to_date, ' ', to_time)  as datetime) <= %s", (equipment, str(posting_datetime) ), as_dict=True)
-# 	if pol:
-# 		return pol[0].total
-# 	else:
-# 		return 0	
+	if not filter_dry:
+		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and include_km=1 and equipment = %s and cast(concat(to_date, ' ', to_time) as datetime) <= %s", (equipment, str(posting_datetime)), as_dict=True)
+	else:
+		pol = frappe.db.sql("select sum(consumption) as total from `tabVehicle Logbook` where docstatus = 1 and include_km=1 and equipment = %s and rate_type = 'With Fuel' and cast(concat(to_date, ' ', to_time)  as datetime) <= %s", (equipment, str(posting_datetime) ), as_dict=True)
+	if pol:
+		return pol[0].total
+	else:
+		return 0	
 
 def get_km_till(equipment, posting_date):
 	if not equipment or not posting_date:
