6671bcd6a3116edf6f112b8bac95816e820252dc
Rinzin <rinzinn1996@gmail.com>
2025-05-05 14:27:13 +0600

    updated

--- erpnext/buying/report/asset_balance_report/asset_balance_report.py
+++ erpnext/buying/report/asset_balance_report/asset_balance_report.py
@@ -80,7 +80,7 @@ def get_data(filters):
 					FROM `tabAsset Issue Details` ai
 					WHERE ai.item_code = ar.item_code
 					AND ai.issued_date BETWEEN '{from_date}' AND '{to_date}' 
-					AND ai.branch = ar.branch
+					AND ai.asset_received_entries = ar.name
 					AND ai.docstatus = 1
 					),0) issued_amount
 			FROM `tabAsset Received Entries` ar
