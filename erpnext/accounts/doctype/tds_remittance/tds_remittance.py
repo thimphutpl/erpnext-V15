09afc0386a1f607c022d2b5c972297b711d7bc73
Rinzin <rinzinn1996@gmail.com>
2025-05-08 11:28:36 +0600

    updated

--- erpnext/accounts/doctype/tds_remittance/tds_remittance.py
+++ erpnext/accounts/doctype/tds_remittance/tds_remittance.py
@@ -194,7 +194,7 @@ def get_tds_invoices(tax_withholding_category, from_date, to_date, name, filter_
 				(select supplier_tpn_no from `tabSupplier` where name = t.supplier) as tpn, 
 				t.cost_center,
 				t1.base_total + t1.base_tax_amount as bill_amount, 
-				(case when t1.base_tax_amount > 0 then t1.base_tax_amount else t1.tax_amount end) as tds_amount,
+				t.taxes_and_charges_deducted as tds_amount,
 				t1.account_head as tax_account, 
 				tre.tds_remittance, 
 				tre.tds_receipt_update, 
@@ -214,6 +214,7 @@ def get_tds_invoices(tax_withholding_category, from_date, to_date, name, filter_
 			{cond}
 		""".format(accounts_cond=accounts_cond, cond=cond, existing_cond=existing_cond,
 				from_date=from_date, to_date=to_date), as_dict=True)
+	# replace (case when t1.base_tax_amount > 0 then t1.base_tax_amount else t1.tax_amount end) as tds_amount in  PI if tax amount is workng
 	#Mechanical Payment
 	if party_type:
 		party_cond = "and t.party_type = '{}'".format(party_type)
