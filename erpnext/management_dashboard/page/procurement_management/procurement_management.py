2b5ab1c3ec18b6c8a7cfe4b6365d2e02529601e3
Rinzin <rinzinn1996@gmail.com>
2025-05-13 15:20:10 +0600

    updated

---
+++ erpnext/management_dashboard/page/procurement_management/procurement_management.py
@@ -0,0 +1,19 @@
+# You'll need to install PyJWT via pip 'pip install PyJWT' or your project packages file
+
+
+import jwt
+import time
+
+METABASE_SITE_URL = "https://erp.ns.bt/metabase"
+METABASE_SECRET_KEY = "f71dae595b626196b8ba5394e2f18646d14cc7b17fb859edfc15d2b330ef72ae"
+
+payload = {
+  "resource": {"dashboard": 4},
+  "params": {
+    
+  },
+#   "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+}
+token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+
+iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#theme=night&bordered=true&titled=true"
