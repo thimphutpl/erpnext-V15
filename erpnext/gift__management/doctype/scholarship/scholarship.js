013a29e297eb7c8c40f9342864b12b2fdf022454
Sonam-555 <sonamdoji384@gmail.com>
2025-05-14 14:33:44 +0600

    updated scholarship.js

--- erpnext/gift__management/doctype/scholarship/scholarship.js
+++ erpnext/gift__management/doctype/scholarship/scholarship.js
@@ -8,31 +8,31 @@
 // });
 
 frappe.ui.form.on('Scholarship', {
-    refresh: function(frm) {
-        // Trigger when the form is refreshed
-        frm.fields_dict['course'].df.onchange = function() {
-            let selected_course = frm.doc.course;
-            let options = frm.fields_dict['course'].df.options.split("\n");
+    // refresh: function(frm) {
+    //     // Trigger when the form is refreshed
+    //     frm.fields_dict['course'].df.onchange = function() {
+    //         let selected_course = frm.doc.course;
+    //         let options = frm.fields_dict['course'].df.options.split("\n");
 
-            if (!options.includes(selected_course)) {
-                frappe.prompt([
-                    {
-                        label: 'New Course',
-                        fieldname: 'new_course',
-                        fieldtype: 'Data',
-                        reqd: 1
-                    }
-                ],
-                function(data) {
-                    // Add the new course to the dropdown options
-                    frm.fields_dict['course'].df.options += '\n' + data.new_course;
-                    frm.set_value('course', data.new_course);
-                    frm.refresh_field('course');
-                },
-                'Add New Course',
-                'Add');
-            }
-        };
-    }
+    //         if (!options.includes(selected_course)) {
+    //             frappe.prompt([
+    //                 {
+    //                     label: 'New Course',
+    //                     fieldname: 'new_course',
+    //                     fieldtype: 'Data',
+    //                     reqd: 1
+    //                 }
+    //             ],
+    //             function(data) {
+    //                 // Add the new course to the dropdown options
+    //                 frm.fields_dict['course'].df.options += '\n' + data.new_course;
+    //                 frm.set_value('course', data.new_course);
+    //                 frm.refresh_field('course');
+    //             },
+    //             'Add New Course',
+    //             'Add');
+    //         }
+    //     };
+    // }
 });
 
