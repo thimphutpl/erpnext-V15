cd1a5fe97b3e126327ba5030278e1775e7698934
Karma <ktenzleo@gmail.com>
2025-05-27 15:09:05 +0600

    updated MR unfreezes

--- erpnext/projects/doctype/muster_roll_employee/muster_roll_employee.py
+++ erpnext/projects/doctype/muster_roll_employee/muster_roll_employee.py
@@ -116,47 +116,46 @@ class MusterRollEmployee(Document):
 			# Fetching previous document from db
 			prev_doc = frappe.get_doc(self.doctype,self.name)
 			self.date_of_transfer = self.date_of_transfer if self.date_of_transfer else today()
-			
 			if (getdate(self.joining_date) != prev_doc.joining_date) or \
 			   (self.status == 'Left' and self.separation_date) or \
 			   (self.cost_center != prev_doc.cost_center):
-				for wh in self.internal_work_history:
-					# For change in joining_date
+				for idx, wh in enumerate(self.internal_work_history):
 					if (getdate(self.joining_date) != prev_doc.joining_date):
-						if (getdate(prev_doc.joining_date) == getdate(wh.from_date)):
+						if (
+							idx == len(self.internal_work_history) - 1 and
+							idx != 0 and
+							wh.to_date is None and
+							(getdate(prev_doc.joining_date) == getdate(wh.from_date)) and
+							all(getdate(wh2.from_date) != getdate(self.joining_date) for i2, wh2 in enumerate(self.internal_work_history) if i2 < idx and i2 != 0)
+						):
 							wh.from_date = self.joining_date
-
-					# For change in separation_date, cost_center
 					if (self.status == 'Left' and self.separation_date):
 						if not wh.to_date:
 							wh.to_date = self.separation_date
 							if wh.from_date > wh.to_date:
 								frappe.throw("To date cannot be before From Date (Separation Date)")
-
-						elif prev_doc.separation_date:
-							if (getdate(prev_doc.separation_date) == getdate(wh.to_date)):
-								wh.to_date = self.separation_date
-					elif (self.cost_center != prev_doc.cost_center):
-						if getdate(self.date_of_transfer) > getdate(today()):
-							frappe.throw(_("Date of transfer cannot be a future date."),title="Invalid Date")      
-						elif not wh.to_date:
-							if getdate(self.date_of_transfer) < getdate(wh.from_date):
-								frappe.throw(_("Row#{0} : Date of transfer({1}) cannot be beyond current effective entry.").format(wh.idx,self.date_of_transfer),title="Invalid Date")
-								
-							wh.to_date = wh.from_date if add_days(getdate(self.date_of_transfer),-1) < getdate(wh.from_date) else add_days(self.date_of_transfer,-1)
-						
-			if ((self.cost_center != prev_doc.cost_center) or (prev_doc.status == 'Left' and self.status == 'Active')):
-				self.append("internal_work_history",{
-						"branch": self.branch,
-						"cost_center": self.cost_center,
-						"from_date": self.date_of_transfer,
-						"owner": frappe.session.user,
-						"creation": nowdate(),
-						"modified_by": frappe.session.user,
-						"modified": nowdate(),
-						"reference_doctype": self.temp_doctype,
-						"reference_docname": self.temp_docname
-				})
+					elif prev_doc.separation_date:
+						if (getdate(prev_doc.separation_date) == getdate(wh.to_date)):
+							wh.to_date = self.separation_date
+				if (self.cost_center != prev_doc.cost_center):
+					if getdate(self.date_of_transfer) > getdate(today()):
+						frappe.throw(_("Date of transfer cannot be a future date."),title="Invalid Date")      
+					elif not wh.to_date:
+						if getdate(self.date_of_transfer) < getdate(wh.from_date):
+							frappe.throw(_("Row#{0} : Date of transfer({1}) cannot be beyond current effective entry.").format(wh.idx,self.date_of_transfer),title="Invalid Date")
+						wh.to_date = wh.from_date if add_days(getdate(self.date_of_transfer),-1) < getdate(wh.from_date) else add_days(self.date_of_transfer,-1)
+		if 'prev_doc' in locals() and ((self.cost_center != prev_doc.cost_center) or (prev_doc.status == 'Left' and self.status == 'Active')):
+			self.append("internal_work_history",{
+					"branch": self.branch,
+					"cost_center": self.cost_center,
+					"from_date": self.date_of_transfer,
+					"owner": frappe.session.user,
+					"creation": nowdate(),
+					"modified_by": frappe.session.user,
+					"modified": nowdate(),
+					"reference_doctype": self.temp_doctype,
+					"reference_docname": self.temp_docname
+			})
 	#added by cety
 	def get_wage_history(self):
 		rate = 0
