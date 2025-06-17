7cef48820d860ae46182353e8d39653345fa618b
SangaTenzin <tenzinsanga1@gmail.com>
2025-06-17 18:20:58 +0600

    Item wise purchase register

--- erpnext/accounts/report/item_wise_purchase_register/item_wise_purchase_register.py
+++ erpnext/accounts/report/item_wise_purchase_register/item_wise_purchase_register.py
@@ -266,7 +266,7 @@ def get_columns(additional_table_columns, filters):
 			"width": 100,
 		},
         {
-                "label": _("Actuali Rate"),
+                "label": _("Actual Rate"),
                 "fieldname": "base_rate",
                 "fieldtype": "Float",
                 "options": "currency",
