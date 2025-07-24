9c86c1dd7a3f0016027e7f26dc343eea9514f715
Sonam-555 <sonamdoji384@gmail.com>
2025-07-24 10:43:11 +0600

    Update Employee

--- erpnext/setup/doctype/employee/employee.py
+++ erpnext/setup/doctype/employee/employee.py
@@ -264,12 +264,33 @@ def get_employee_email(employee_doc):
 	)
 
 
+# def get_holiday_list_for_employee(employee, raise_exception=True):
+# 	if employee:
+# 		holiday_list, company = frappe.get_cached_value("Employee", employee, ["holiday_list", "company"])
+# 	else:
+# 		holiday_list = ""
+# 		company = frappe.db.get_single_value("Global Defaults", "default_company")
+
+# 	if not holiday_list:
+# 		holiday_list = frappe.get_cached_value("Company", company, "default_holiday_list")
+
+# 	if not holiday_list and raise_exception:
+# 		frappe.throw(
+# 			_("Please set a default Holiday List for Employee {0} or Company {1}").format(employee, company)
+# 		)
+
+# 	return holiday_list
+
 def get_holiday_list_for_employee(employee, raise_exception=True):
 	if employee:
-		holiday_list, company = frappe.get_cached_value("Employee", employee, ["holiday_list", "company"])
+		holiday_list, company = frappe.db.get_value("Employee", employee, ["holiday_list", "company"])
 	else:
 		holiday_list = ""
 		company = frappe.db.get_single_value("Global Defaults", "default_company")
+	
+	if not holiday_list:
+		holiday_list = frappe.db.get_value("Branch", frappe.db.get_value("Employee", employee, "branch"), "holiday_list")
+		
 
 	if not holiday_list:
 		holiday_list = frappe.get_cached_value("Company", company, "default_holiday_list")
