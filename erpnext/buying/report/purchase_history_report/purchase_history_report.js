8ca9cab90241bcce160bcd3bd2d0d0906dda6f67
pemanorbu2 <pemanorbu132@gmail.com>
2025-07-25 11:40:00 +0600

    added new filter at purchase history report

--- erpnext/buying/report/purchase_history_report/purchase_history_report.js
+++ erpnext/buying/report/purchase_history_report/purchase_history_report.js
@@ -40,5 +40,15 @@ frappe.query_reports["Purchase History Report"] = {
             "options": "Purchase Invoice",
             "default": "" 
         }
-    ]
+    ],
+    	onload: function(report) {
+        report.page.add_inner_button("Clear Filters", function () {
+            report.set_filter_value("from_date", null);
+            report.set_filter_value("to_date", null);
+            report.set_filter_value("mr_name", null);
+            report.set_filter_value("po_name", null);
+            report.set_filter_value("pr_name", null);
+            report.set_filter_value("pi_name", null);
+        });
+    },
 };
