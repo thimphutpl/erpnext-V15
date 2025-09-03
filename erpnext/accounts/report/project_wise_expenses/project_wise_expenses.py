2e248bbd77fd29f1ce50578d5a8c66f78149ebd8
Rinzin <rinzinn1996@gmail.com>
2025-09-03 15:07:06 +0600

    updated

--- erpnext/accounts/report/project_wise_expenses/project_wise_expenses.py
+++ erpnext/accounts/report/project_wise_expenses/project_wise_expenses.py
@@ -5,109 +5,68 @@ import frappe
 from frappe.utils import flt
 
 def execute(filters=None):
-	columns = get_columns()
-	data = get_data(filters)
-	return columns, data
+    validate_filters(filters)
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def validate_filters(filters):
+    if not filters:
+        frappe.throw("Filters are required.")
+    if not filters.get("from_date") or not filters.get("to_date"):
+        frappe.throw("Please provide From Date and To Date.")
 
 def get_data(filters=None):
-	data = []
-	cond = ''
-	if filters.project_definition:
-		cond += " and project_definition = %s" % frappe.db.escape(filters.project_definition)
-	project_data = frappe.db.sql("""select name, cost_center, project_definition 
-		from `tabProject` 
-		where docstatus != 2 
-		{cond}
-	""".format(cond=cond),as_dict=True)
+    gl_cond = "and gl.account = %s" if filters.account else ""
+    query = f"""
+        SELECT 
+            p.name AS project, 
+            p.cost_center, 
+            p.project_definition, 
+            SUM(COALESCE(gl.debit, 0)) AS expense, 
+            SUM(COALESCE(gl.credit, 0)) AS income
+        FROM `tabProject` p
+        LEFT JOIN `tabGL Entry` gl ON gl.project = p.name
+        LEFT JOIN `tabAccount` acc ON acc.name = gl.account
+        WHERE p.docstatus != 2
+        AND acc.root_type = 'Expense'
+        AND gl.is_cancelled = 0
+		AND p.project_definition = %s
+        AND gl.posting_date BETWEEN %s AND %s
+        {gl_cond}
+        GROUP BY p.name
+    """
+    params = [filters.project_definition, filters.from_date, filters.to_date]
+    if filters.account:
+        params.append(filters.account)
 
-	for raw in project_data:
-		total_debit, total_credit = 0, 0
-		gl_cond = "and gl.account = %s" if filters.account else ""
+    data = frappe.db.sql(query, tuple(params), as_dict=True)
+    return [
+        build_row(row, filters)
+        for row in data
+    ]
 
-		overall_expense = frappe.db.sql(
-			"""select sum(gl.debit) as expense, sum(gl.credit) as income 
-				from `tabGL Entry` gl
-				inner join `tabAccount` acc on acc.name = gl.account
-				where is_cancelled = 0
-				and acc.root_type = 'Expense'
-				and posting_date between %s and %s
-				and project = %s
-				{cond}""".format(cond=gl_cond),
-			(filters.from_date, filters.to_date, raw.name, filters.account) if filters.account else (filters.from_date, filters.to_date, raw.name),
-			as_dict=True
-		)
+def build_row(raw, filters):
+    total_expense = flt(raw.expense or 0) - flt(raw.income or 0)
+    return frappe._dict({
+        "cost_center": raw.cost_center,
+        "project": raw.project,
+        "expense": raw.expense or 0,
+        "income": raw.income or 0,
+        "total_expense": total_expense,
+        "from_date": filters.from_date,
+        "to_date": filters.to_date
+    })
 
-		if overall_expense:
-			total_debit = overall_expense[0].get("expense", 0)
-			total_credit = overall_expense[0].get("income", 0)
-
-		difference = flt(total_debit) - flt(total_credit)
-
-		row = frappe._dict({
-			"cost_center": raw.cost_center,
-			"project": raw.name,
-			"expense": total_debit,
-			"income": total_credit,
-			"total_expense": difference,
-			"from_date": filters.from_date,
-			"to_date": filters.to_date
-		})
-		data.append(row)
-
-	return data
-
-def build_condition(filters):
-	 cond = ""
-	 if filters.project_definition:
-		  cond += "and project_definition = %s"
-	 return cond
+COLUMN_DEFINITIONS = [
+    {"fieldname": "cost_center", "fieldtype": "Link", "width": 250, "label": "Cost Center", "options": "Cost Center"},
+    {"fieldname": "project", "fieldtype": "Link", "width": 350, "label": "Project", "options": "Project"},
+    {"fieldname": "expense", "fieldtype": "Data", "width": 150, "label": "Expense"},
+    {"fieldname": "income", "fieldtype": "Data", "width": 150, "label": "Income"},
+    {"fieldname": "total_expense", "fieldtype": "Data", "width": 150, "label": "Total Expense"},
+    {"fieldname": "from_date", "fieldtype": "Date", "width": 120, "label": "From Date"},
+    {"fieldname": "to_date", "fieldtype": "Date", "width": 120, "label": "To Date"}
+]
 
 def get_columns():
-	return [
-		{	
-			"fieldname":"cost_center",
-			"fieldtype":"Link",
-			"width":250,
-			"label":"Cost Center",
-			"options":"Cost Center"
-		},
-		{	
-			"fieldname":"project",
-			"fieldtype":"Link",
-			"width":250,
-			"label":"Project",
-			"options":"Project"
-		},
-		{
-			"fieldname":"expense",
-			"fieldtype":"Data",
-			"width":150,
-			"label":"Expense"
-		},
-		{
-			"fieldname":"income",
-			"fieldtype":"Data",
-			"width":150,
-			"label":"Income"
-		},
-		{
-			"fieldname":"total_expense",
-			"fieldtype":"Data",
-			"width":150,
-			"label":"Total Expense"
-		},
-		{
-			"fieldname":"from_date",
-			"fieldtype":"Date",
-			"width":120,
-			"label":"From Date"
-		},
-		{
-			"fieldname":"to_date",
-			"fieldtype":"Date",
-			"width":120,
-			"label":"To Date"
-		}
-	]
-
-
+    return COLUMN_DEFINITIONS
