b7f1dd31e3c628c4d4b7760a9b43596ad66d56a7
TW <tw@gmail.com>
2025-11-24 18:33:28 +0600

    pol advance

--- erpnext/fleet_management/doctype/pol_receive/pol_receive.py
+++ erpnext/fleet_management/doctype/pol_receive/pol_receive.py
@@ -180,8 +180,9 @@ class POLReceive(StockController):
 		self.make_pol_entry()
 		# if self.settled_using_imprest:
 		# 	self.make_gl_entry()
-		if self.direct_consumption:
-			self.post_journal_entry()
+		# if self.direct_consumption:
+		# 	self.post_journal_entry()
+		self.post_journal_entry()
 
 	def on_cancel(self):
 		self.ignore_linked_doctypes = (
@@ -424,6 +425,7 @@ class POLReceive(StockController):
 		# 	frappe.throw("Can not determine machine category")
 
 		expense_bank_account = frappe.db.get_value("Company", self.company, "pol_advance_account")
+		pol_account = frappe.db.get_value("Company", self.company, "pol_expense_account")
 		
 		if not expense_bank_account:
 			frappe.throw("No Default Payable Account set in Company")
@@ -432,13 +434,13 @@ class POLReceive(StockController):
 			je = frappe.new_doc("Journal Entry")
 			je.flags.ignore_permissions = 1 
 			je.title = "POL Receive (" + self.pol_type + " for " + self.equipment_number + ")"
-			je.voucher_type = 'Bank Entry'
-			je.naming_series = 'Bank Payment Voucher'
+			je.voucher_type = 'Journal Entry'
+			je.naming_series = 'Journal Voucher'
 			je.remark = 'Payment against : ' + self.name;
 			je.posting_date = self.posting_date
 			je.branch = self.branch
-			je.company = self.company,
-			je.mode_of_payment = 'ePayment',
+			je.company = self.company
+			# je.mode_of_payment = 'ePayment',
 
 			je.append("accounts", {
 					"account": pol_account,
@@ -461,6 +463,7 @@ class POLReceive(StockController):
 				})
 
 			je.insert()
+			je.submit()
 			self.db_set("jv", je.name)
 			frappe.msgprint(_('Journal Entry {} posted to accounts').format(frappe.get_desk_link(je.doctype,je.name)))
 
