7a64f64e48d30ee6a6d77c96c2aa21e7e74932ba
Tw <tw@gmail.com>
2026-01-04 01:35:53 +0600

    tw

---
+++ erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CRMBranchSetting(Document):
+	def autoname(self):
+		self.name = self.branch+" ("+self.product_category+")"
+		if frappe.db.exists("CRM Branch Setting",self.name):
+			self.name = make_autoname(self.branch+" ("+self.product_category+ ") .####")
