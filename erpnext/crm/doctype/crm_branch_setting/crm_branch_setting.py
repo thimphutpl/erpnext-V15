92444f62ab31f972c4d9a932b8648e694253004a
Tw <tw@gmail.com>
2026-01-04 01:38:45 +0600

    tw

--- erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.py
+++ erpnext/crm/doctype/crm_branch_setting/crm_branch_setting.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
