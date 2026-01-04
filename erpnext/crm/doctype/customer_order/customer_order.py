9ea9e32193978b8140eab8a96e074969ffa74c03
Tw <tw@gmail.com>
2026-01-04 19:15:27 +0600

    tw

--- erpnext/crm/doctype/customer_order/customer_order.py
+++ erpnext/crm/doctype/customer_order/customer_order.py
@@ -105,10 +105,10 @@ class CustomerOrder(Document):
 		yearly_quantity_limit_count: DF.Int
 	# end: auto-generated types
 	def validate(self):
-		if self.product_category in ("Sand", "product_category"):
-			self.sales_order_series = "Mineral Products"
+		if self.product_category in ("Sand", "Boulders and Aggregates"):
+    		self.sales_order_series = "Mineral Products"
 		else:
-			self.sales_order_series = "Timber Products"
+    		self.sales_order_series = "Timber Products"
 
 		
 		self.check_for_duplicates()
@@ -145,10 +145,9 @@ class CustomerOrder(Document):
 						and co.product_category="Sand"
 						and co.site="{}"
 						""".format(self.customer, self.item, self.site), as_dict=True):
-			pass
-			# if a.name:
-			# 	# frappe.throw(str(a.name))
-			# 	frappe.throw("Making order not allowed for {} as the previous order {} is still not delivered".format(self.product_category, a.name))
+			if a.name:
+				# frappe.throw(str(a.name))
+				frappe.throw("Making order not allowed for {} as the previous order {} is still not delivered".format(self.product_category, a.name))
 
 	def before_submit(self):
 		self.posting_date = now()
