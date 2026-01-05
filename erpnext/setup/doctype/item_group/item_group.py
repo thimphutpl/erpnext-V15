b3713631c3b3befe15f4c0de18e965696fa63ca9
SangaTenzin <tenzinsanga1@gmail.com>
2026-01-05 14:59:28 +0600

    Item Group

--- erpnext/setup/doctype/item_group/item_group.py
+++ erpnext/setup/doctype/item_group/item_group.py
@@ -20,6 +20,7 @@ class ItemGroup(NestedSet):
 		from erpnext.stock.doctype.item_tax.item_tax import ItemTax
 		from frappe.types import DF
 
+		can_be_capitalized: DF.Check
 		image: DF.AttachImage | None
 		is_fixed_asset: DF.Check
 		is_group: DF.Check
