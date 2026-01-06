2dcb266c44ad6ab5c3478ee10025c484cefe5b16
kinleydorji <kd77144@gmail.com>
2026-01-06 15:57:30 +0600

    department

--- erpnext/setup/doctype/department/department.py
+++ erpnext/setup/doctype/department/department.py
@@ -103,3 +103,23 @@ def add_node():
 		args.parent_department = None
 
 	frappe.get_doc(args).insert()
+
+@frappe.whitelist()
+def get_employee_count(department):
+	dep = frappe.get_doc("Department", department)
+	cond = ''
+	if cint(dep.is_department):	
+		cond = ' and department ="{}"'.format(dep.name)
+	if cint(dep.is_division):	
+		cond = ' and division ="{}"'.format(dep.department_name)
+	if cint(dep.is_unit): 
+		cond = ' and unit ="{}"'.format(dep.department_name)
+	if cint(dep.is_section): 
+		cond = ' and section ="{}"'.format(dep.department_name)
+	data = {}
+	res = frappe.db.sql("""select count(*) employee_count from `tabEmployee`
+					where status = "Active" {} """.format(cond))
+	data['count'] = res[0][0] if res[0][0] else 0
+	data['approver_name'] = dep.approver_name
+	data['approver_level'] = dep.approver_level
+	return data
--- erpnext/setup/doctype/department/department_tree.js
+++ erpnext/setup/doctype/department/department_tree.js
@@ -1,29 +1,65 @@
 frappe.treeview_settings["Department"] = {
-	ignore_fields: ["parent_department"],
-	get_tree_nodes: "erpnext.setup.doctype.department.department.get_children",
-	add_tree_node: "erpnext.setup.doctype.department.department.add_node",
+	ignore_fields:["parent_department"],
+	get_tree_nodes: 'erpnext.setup.doctype.department.department.get_children',
+	add_tree_node: 'erpnext.setup.doctype.department.department.add_node',
 	filters: [
 		{
 			fieldname: "company",
-			fieldtype: "Link",
+			fieldtype:"Link",
 			options: "Company",
 			label: __("Company"),
-			default: erpnext.utils.get_tree_default("company"),
 		},
 	],
+	field: ["department_name", "is_division"],
 	breadcrumb: "HR",
 	root_label: "All Departments",
 	get_tree_root: true,
 	menu_items: [
 		{
 			label: __("New Department"),
-			action: function () {
+			action: function() {
 				frappe.new_doc("Department", true);
 			},
-			condition: 'frappe.boot.user.can_create.indexOf("Department") !== -1',
-		},
+			condition: 'frappe.boot.user.can_create.indexOf("Department") !== -1'
+		}
 	],
-	onload: function (treeview) {
+	onload: function(treeview) {
 		treeview.make_tree();
 	},
+	onrender: function(node){
+		get_identifier(node)
+		// get_employee_count(node);
+	},
 };
+
+function get_identifier(node){
+	let label = ''
+	if(node.data.value == 'Chief Executive Officer'){
+		label = 'CEO'
+	}
+	frappe.call({
+		method: "erpnext.setup.doctype.department.department.get_employee_count",
+		args: {
+			department: node.data.value
+		},
+		callback: function(r){
+			let format_string1 = ""
+			let format_string2 = ""
+			let format_string3 = ""
+			let border1 = "3px 0px 0px 3px"
+			let border2 = "3px 3px 3px 3px"
+			if (r.message.approver_level) {
+				format_string1 = `<span class="badge badge-light" style="font-size:xx-small; margin-left: 5px; border-right: 1px solid grey; border-radius: 3px 0px 0px 3px; background-color: #f68446">${r.message.approver_level}</span>`;
+				border1 = "0px 0px 0px 0px"
+			}
+			if (r.message.approver_name) {
+				format_string2 = `<span class="badge badge-light" style="font-size:xx-small; border-right: 1px solid grey; border-radius: ${border1}; background-color: #e5c9f7">${r.message.approver_name}</span>`;
+				border2 = "0px 3px 3px 0px"
+			}
+			format_string3 = `<span class="badge badge-light" style="font-size:xx-small; border-radius: ${border2}; background-color: gold">${r.message.count}</span>`;
+			node.$tree_link.after(format_string1+format_string2+format_string3)
+
+		}
+	})
+	
+}
