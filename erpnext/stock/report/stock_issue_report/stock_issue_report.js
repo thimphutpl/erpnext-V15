3713daea18cd3281f2cbb2d5f08e563c1e5de5c6
Lhendup97 <k.lhendup97@gmail.com>
2026-01-27 15:00:48 +0600

    stock report

---
+++ erpnext/stock/report/stock_issue_report/stock_issue_report.js
@@ -0,0 +1,173 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Stock Issue Report"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": ("Company"),
+			"fieldtype": "Link",
+ 			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"purpose",
+			"label": __("Purpose"),
+			"fieldtype": "Select",
+			"width": "80",
+			"options": ["Material Issue", "Material Transfer"],
+			"reqd": 1
+		},
+		{
+            "fieldname": "cost_center",
+            "label": __("Parent Cost Center"),
+			"fieldtype": "Link",
+            "width": "80",
+			"options": "Cost Center",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value("company");
+				return {
+					filters: {
+						disabled: 0,
+						company: company,
+						is_group: 1
+					}
+				};
+			},
+			"on_change": function(){
+				var cost_center = frappe.query_report.get_filter_value("cost_center");
+				var from_date = frappe.query_report.get_filter_value("from_date");
+				var to_date = frappe.query_report.get_filter_value("to_date");
+				if(cost_center)
+				{
+					frappe.call({
+						method:"erpnext.stock.report.stock_balance_report.stock_balance_report.get_warehouse",
+						args:{"cost_center":cost_center, "from_date":from_date, "to_date": to_date},
+						callback: function(r){
+							if(r.message)
+							{
+								options = []
+								for (i = 0; i < r.message.length; i++) { 
+									options[i]= r.message[i].warehouse
+								}
+								let warehouse = frappe.query_report.get_filter_value("warehouse");
+								warehouse.df.options = options;
+								warehouse.refresh();
+								frappe.query_report.refresh();
+
+								// frappe.query_report.filters_by_name.challan_no.refresh();
+								// frappe.query_reports["Production Report"].filters[19].options = options
+								// frappe.query_report.filters_by_name.challan_no.refresh();
+								// frappe.query_report.refresh();
+							}
+						}
+						
+					});
+				}
+			},
+		},
+		{
+			"fieldname": "branch",
+			"label": ("Branch"),
+			"fieldtype": "Link",
+			"options": "Cost Center",
+			get_query: () => {
+				var cost_center = frappe.query_report.get_filter_value("cost_center")
+				var company = frappe.query_report.get_filter_value("company")
+				if(cost_center!= 'Natural Resource Development Corporation Ltd - NRDCL')
+				{
+					return {
+						filters: {
+							disabled: 0,
+							company: company,
+							parent_cost_center: cost_center
+						}
+					};
+				}
+				else
+				{
+					return {
+						filters: {
+							disabled: 0,
+							company: company,
+							is_group: 0
+						}
+					};
+				}
+			},
+			"on_change": function(){
+				var branch = frappe.query_report.get_filter_value("branch");
+				frappe.call({
+					method:"erpnext.stock.report.stock_balance_report.stock_balance_report.get_warehouse",
+					args:{"branch":branch, "from_date":from_date, "to_date": to_date},
+					callback: function(r){
+						if(r.message)
+						{
+							options = []
+							for (i = 0; i < r.message.length; i++) { 
+								options[i]= r.message[i].warehouse
+							}
+							
+							let warehouse = frappe.query_report.get_filter_value("warehouse");
+							warehouse.df.options = options;
+							warehouse.refresh();
+							frappe.query_report.refresh();
+						}
+					}
+				})
+
+			}
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.year_start(),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname": "warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Select",
+			"width": "80",
+			"options": [""]
+		},
+
+		{
+			"fieldname": "item_code",
+			"label": __("Material Code"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Item"
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Material Group"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Item Group"
+		},
+		{
+			"fieldname": "uom",
+			"label": ("UOM"),
+			"fieldtype": "Link",
+			"options": "UOM"
+		},
+		{
+			"fieldname": "lot_number",
+			"label": __("Lot Number"),
+			"fieldtype": "Link",
+			"width": "100",
+			"options": "Lot List"
+		}
+
+	]
+}
---
+++ erpnext/stock/report/stock_receipt_report/stock_receipt_report.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import flt, cint,add_days, cstr, flt, getdate, nowdate, rounded, date_diff
+from erpnext.accounts.utils import get_child_cost_centers
+
+def execute(filters=None):
+	columns = get_columns(filters)
+	data = get_data(filters)
+	return columns, data
+
+
+def get_columns(filters):
+	cols = [
+		("Date") + ":date:100",
+		("Material Code") + ":data:110",
+		("Material Name")+":data:120",
+		("Material Group")+":data:120",
+		("Material Sub Group")+":data:150",
+		("To Warehouse")+":data:150",
+		("UoM") + ":data:50",
+		("Qty")+":Float:50",
+		("Valuation Rate")+":data:120",
+		("Amount")+":Currency:110",
+		("Lot Number")+ ":data:100",
+		("From Warehouse")+":data:170"
+
+	]
+	cols.append(("Stock Entry")+":Link/Stock Entry:170")
+	return cols
+
+def get_data(filters):
+	data = "select se.posting_date, sed.item_code, sed.item_name, (select i.item_group from tabItem i where i.item_code = sed.item_code) as item_group, (select i.item_sub_group from tabItem i where i.item_code = sed.item_code) as item_sub_group, sed.t_warehouse, sed.uom, sed.qty, sed.valuation_rate,sed.amount, sed.lot_number, sed.s_warehouse, se.name FROM `tabStock Entry` se, `tabStock Entry Detail` sed WHERE se.name = sed.parent and  se.docstatus = 1"
+	if filters.cost_center:
+		if not filters.get("branch"):
+			all_ccs = get_child_cost_centers(filters.cost_center)
+			data += " and se.to_warehouse in (select parent from `tabWarehouse Branch` wb where wb.branch in (select name from `tabBranch` b where b.cost_center in {0}))".format(tuple(all_ccs))
+		else:
+			branch_name = frappe.db.get_value("Branch",{"cost_center": filters.get("branch")}, "name")
+			data += " and se.to_warehouse in (select parent from `tabWarehouse Branch` wb where wb.branch in (select name from `tabBranch` b where b.name = '{0}'))".format(branch_name)
+	if filters.get("purpose") == "Material Receipt":
+		data += " and se.purpose = 'Material Receipt'"
+	if filters.get("purpose") == "Material Transfer":
+		data += " and se.purpose = 'Material Transfer'"
+	if filters.get("warehouse"):
+		data += " and sed.t_warehouse = \'" + str(filters.warehouse) + "\'"
+	if filters.get("item_code"):
+		data += " and sed.item_code = \'" + str(filters.item_code) + "\'"
+	if filters.get("item_group"):
+		data += " and sed.item_group = \'" + str(filters.item_group) + "\'"
+	if filters.get("from_date") and filters.get("to_date"):
+		data += " and se.posting_date between \'" + str(filters.from_date) + "\' and \'"+ str(filters.to_date) + "\'"
+	if filters.lot_number:
+		data += " and sed.lot_number = \'" + str(filters.lot_number) + "\'"
+	if filters.item_group:
+		data += " and exists(select i.item_group from tabItem i where i.item_code = sed.item_code and i.item_group = \'"+str(filters.item_group)+"\')"
+	if filters.uom:
+		data += " and sed.uom = \'" + str(filters.uom) + "\'"
+	return frappe.db.sql(data)
