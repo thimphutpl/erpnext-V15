66b77757f001ee992b76aea16e405e07e567cc58
karma <ktenzleo@gmail.com>
2026-05-11 10:36:52 +0600

    change in asset balance reports

--- erpnext/buying/report/asset_balance_report/asset_balance_report.py
+++ erpnext/buying/report/asset_balance_report/asset_balance_report.py
@@ -58,7 +58,7 @@ def get_data(filters):
 				SUM(ar.qty) AS received_qty,
 				SUM(
 					(
-						SELECT pri.base_net_rate
+						SELECT pri.base_net_amount
 						FROM `tabPurchase Receipt Item` pri
 						JOIN `tabPurchase Receipt` pr
 							ON pr.name = pri.parent
